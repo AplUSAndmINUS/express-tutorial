f7d3dc48e7cbd1d8de20a509d0f2bc032db6aae5
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2020-12-20 19:49:07 -0700

    added html response for posts

--- index.ts
+++ index.ts
@@ -4,6 +4,20 @@ let express = require("express");
 // calls to set up express server
 let app = express (); 
 
+// submit HTML
 app.get("/", function(request, response) {
-  // submit HTML
+  let html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>My Blog!</title>
+    </head>
+
+    <body>
+    ${posts.map(post => {
+      `<li>${post.title}</li>`
+    }).join('')}</body></html>
+  `;
+
+  response.send(html);
 });
