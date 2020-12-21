fd02f243ea145892bffcd14bc876adcbe78e21b9
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2020-12-20 20:15:22 -0700

    added mysql connection and connectivity

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
-// requirement for express setup
+// requirements for express setup
+// Express.js, MySQL, and database connection
 let express = require("express"); 
+let mysql = require("mysql");
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "P@$$W0RD321!",
+  database: "myblog"
+});
+
+// set up MySQL connection 
+connection.connect();
 
 // calls to set up express server
-let app = express (); 
+let app = express(); 
 
 // submit HTML
 app.get("/", function(request, response) {
