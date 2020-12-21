80aeef3407404c4c8339cb73f83f28dfc18be3e3
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2020-12-20 17:02:51 -0700

    initial setup of the web server in express

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
-let express = require("express");
-let app = express ();
+// requirement for express setup
+let express = require("express"); 
+
+// calls to set up express server
+let app = express (); 
+
+app.get("/", function(request, response) {
+  // submit HTML
+});
